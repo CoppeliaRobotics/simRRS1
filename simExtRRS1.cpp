32b4460bfa34c3f0510ff41781847b2af60b4547
Coppelia <mfreese@gmx.ch>
2020-05-15 09:35:32 +0200

    simAddLog only when CoppeliaSim lib is loaded!

--- simExtRRS1.cpp
+++ simExtRRS1.cpp
@@ -4656,12 +4656,12 @@ SIM_DLLEXPORT unsigned char simStart(void* reservedPointer,int reservedInt)
     simLib=loadSimLibrary(temp.c_str());
     if (simLib==NULL)
     {
-        simAddLog("RRS1",sim_verbosity_errors,"could not find or correctly load the CoppeliaSim library. Cannot start the plugin.");
+        printf("simExtRRS1: error: could not find or correctly load the CoppeliaSim library. Cannot start the plugin.\n"); // cannot use simAddLog here.
         return(0); 
     }
     if (getSimProcAddresses(simLib)==0)
     {
-        simAddLog("RRS1",sim_verbosity_errors,"could not find all required functions in the CoppeliaSim library. Cannot start the plugin.");
+        printf("simExtRRS1: error: could not find all required functions in the CoppeliaSim library. Cannot start the plugin.\n"); // cannot use simAddLog here.
         unloadSimLibrary(simLib);
         return(0);
     }
